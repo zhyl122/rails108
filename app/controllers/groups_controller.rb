fd580797f38ba2990527c15be8c4f1b56c03150c
zhouyilin <zhyl9697@gmail.com>
2017-04-25 16:11:41 +0800

    implement post create function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,6 +9,7 @@ class GroupsController < ApplicationController
 
   def show
     @group = Group.find(params[:id])
+    @posts = @group.posts
   end
 
   def edit
@@ -43,7 +44,7 @@ class GroupsController < ApplicationController
     redirect_to groups_path, alert: "Group deleted"
     end
 
-  
+
 
  private
 
